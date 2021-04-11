3c4f630dc0b295c415489b081d6cdaba7fa3b5c5
DimitriDS <dimitri.gomes.da.silva@gmail.com>
2021-04-11 17:58:47 +0100

    Made find median for linked lists

--- src/linked_lists/ft_lst_find_median.c
+++ src/linked_lists/ft_lst_find_median.c
@@ -6,20 +6,26 @@
 /*   By: dda-silv <dda-silv@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/11 16:10:23 by dda-silv          #+#    #+#             */
-/*   Updated: 2021/04/11 16:24:55 by dda-silv         ###   ########.fr       */
+/*   Updated: 2021/04/11 17:35:55 by dda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_lst_find_median.h"
+#include "libft.h"
 
-long long			ft_lst_find_median(t_list *lst)
+long long	ft_lst_find_median(t_list *lst)
 {
-	t_list *dup;
+	t_list		*dup;
+	t_list		*tmp;
+	int			half_size;
+	long long	median;
 
 	dup = ft_lstdup(lst);
-	ft_lst_print_d(dup);
+	half_size = ft_lstsize(dup) / 2;
 	ft_lst_sort(&dup, ascending);
-	ft_lst_print_d(dup);
+	tmp	= dup;
+	while (half_size--)
+		tmp = tmp->next;
+	median = (long long)tmp->data;
 	ft_lstclear(&dup, ft_lstdel_int);
-	return (3);
+	return (median);
 }
