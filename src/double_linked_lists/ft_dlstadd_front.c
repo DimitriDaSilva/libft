0df457cff2e04ac3143b22811e4c2aaa77d644e8
DimitriDS <dimitri.gomes.da.silva@gmail.com>
2021-04-25 15:30:59 +0100

    Fixed bug in ft_dlstadd_front

--- src/double_linked_lists/ft_dlstadd_front.c
+++ src/double_linked_lists/ft_dlstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: dda-silv <dda-silv@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/15 16:43:23 by dda-silv          #+#    #+#             */
-/*   Updated: 2021/04/25 11:16:46 by dda-silv         ###   ########.fr       */
+/*   Updated: 2021/04/25 15:26:23 by dda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_dlstadd_front(t_dlist **lst, t_dlist *new)
 {
 	new->next = *lst;
 	new->prev = 0;
-	if (!(*lst))
+	if (*lst)
 		(*lst)->prev = new;
 	*lst = new;
 }
