d91d902b059f3071e3d9457ec8afffec6c9565ba
DimitriDS <dimitri.gomes.da.silva@gmail.com>
2021-04-30 10:26:18 +0100

    Delete ft_del_int duplicate

--- src/double_linked_lists/ft_dlstclear.c
+++ src/double_linked_lists/ft_dlstclear.c
@@ -6,7 +6,7 @@
 /*   By: dda-silv <dda-silv@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/15 16:56:31 by dda-silv          #+#    #+#             */
-/*   Updated: 2021/04/25 11:50:29 by dda-silv         ###   ########.fr       */
+/*   Updated: 2021/04/30 10:17:44 by dda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,4 @@ void	ft_dlstclear(t_dlist **lst, void (*del)(void*))
 		ft_dlstdelone(tmp, del);
 	}
 	*lst = 0;
-}
-
-void	ft_lstdel_int(void *data)
-{
-	(void)data;
 }
