6f4164b62094745fbe7448870893d83cd7bbc686
DimitriDS <dimitri.gomes.da.silva@gmail.com>
2021-05-07 11:18:10 +0100

    Protected free_arr

--- src/array/free_arr.c
+++ src/array/free_arr.c
@@ -6,7 +6,7 @@
 /*   By: dda-silv <dda-silv@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/09 08:54:04 by dda-silv          #+#    #+#             */
-/*   Updated: 2021/04/09 19:30:49 by dda-silv         ###   ########.fr       */
+/*   Updated: 2021/05/07 10:33:52 by dda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ void	free_arr(void **ptr)
 {
 	int	i;
 
+	if (!ptr)
+		return ;
 	i = 0;
 	while (ptr[i - 1])
 		free(ptr[i++]);
