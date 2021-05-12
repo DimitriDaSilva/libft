503c5c4537e861dca69b9fcf978a6d1eafab541f
DimitriDS <dimitri.gomes.da.silva@gmail.com>
2021-05-12 18:32:52 +0100

    Added a WHITE_SPACE const

--- src/constants_libft.h
+++ src/constants_libft.h
@@ -6,7 +6,7 @@
 /*   By: dda-silv <dda-silv@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/25 11:08:18 by dda-silv          #+#    #+#             */
-/*   Updated: 2021/04/25 11:54:40 by dda-silv         ###   ########.fr       */
+/*   Updated: 2021/05/12 18:13:27 by dda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,6 @@
 # define ASCII_OFFSET_NUM 48
 # define ASCII_OFFSET_ALPHA 32
 # define MAX_LINE 200000
+# define WHITE_SPACE " \t\n\v\f\r"
 
 #endif
